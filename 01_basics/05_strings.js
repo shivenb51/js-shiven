fe8b833b2eec0b8c79d0ccd1685e10b8cee4a1f3
Shiven Bhanarkar <shivenbhanarkar@gmail.com>
2025-05-26 20:46:52 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -26,7 +26,7 @@ const newStringOne = "   hitesh    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+
 
 console.log(url.replace('%20', '-'))
 
